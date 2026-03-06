16fe364477c9ba1928e9f4698db3f1f5b635b5a8
kcct-ts8 <43575586+kcct-ts8@users.noreply.github.com>
2026-03-06 13:25:06 +0900

    Update keymap.c
    Add my keymap to firmware.

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/test/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/test/keymap.c
@@ -39,15 +39,78 @@ enum my_keycodes {
 static bool is_precision_mode = false;
 
 // clang-format off
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_universal(
-    KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS  ,
-    KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
-    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT  ,
-                    KC_LCTL  , KC_LGUI  , KC_LALT  ,       KC_SPC   , KC_DEL   ,                  KC_BSPC  , KC_ENT   ,      KC_RALT  , KC_RGUI  , KC_RCTL
-  ),
-};
+//const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+//  [0] = LAYOUT_universal(
+//    KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS  ,
+//    KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
+//    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT  ,
+//                    KC_LCTL  , KC_LGUI  , KC_LALT  ,       KC_SPC   , KC_DEL   ,                  KC_BSPC  , KC_ENT   ,      KC_RALT  , KC_RGUI  , KC_RCTL
+//  ),
+//};
 // clang-format on
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  // =========================================================
+  // Layer 0: 基本配列 (Base)
+  // =========================================================
+  [0] = LAYOUT_universal(
+    KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                                      KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_RBRC,
+    KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                                      KC_H   , KC_J   , KC_K   , KC_L   , LT(1, KC_MINS), KC_BSLS,
+    KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                                      KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, TG(1)  ,
+             KC_NO  , LT(3, KC_ESC), LT(1, KC_LALT), KC_LGUI, LT(4, KC_SPC),        MO(5)  , LT(2, KC_ENT), LT(3, KC_LBRC), KC_NO  , KC_NO
+  ),
+
+  // =========================================================
+  // Layer 1: マウス操作・メディア・独自ショートカット
+  // =========================================================
+  [1] = LAYOUT_universal(
+    _______, KC_1   , _______, _______, _______, _______,                                      _______, KC_F17 , KC_F19 , KC_F18 , KC_F13 , KC_F12 ,
+    KC_F23 , KC_F22 , _______, _______, _______, KC_F15 ,                                      KC_BSPC, KC_MS_BTN1, KC_MS_BTN3, KC_MS_BTN2, KC_F16 , KC_F11 ,
+    _______, _______, _______, _______, KC_SPC , KC_F14 ,                                      KC_VOLD, KC_VOLU, _______, KC_MUTE, _______, _______,
+             _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______
+  ),
+
+  // =========================================================
+  // Layer 2: 記号・テンキー補助
+  // =========================================================
+  [2] = LAYOUT_universal(
+    _______, _______, _______, _______, _______, _______,                                      _______, KC_RO  , S(KC_7), S(KC_8), _______, _______,
+    _______, _______, _______, _______, _______, _______,                                      KC_BSPC, KC_PLUS, KC_MINS, S(KC_BSLS),_______, _______,
+    _______, _______, _______, _______, _______, _______,                                      _______, _______, KC_DOT , KC_COMM, _______, _______,
+             _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______
+  ),
+
+  // =========================================================
+  // Layer 3: RGB LED コントロール
+  // =========================================================
+  [3] = LAYOUT_universal(
+    RGB_TOG, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW,_______,                                      _______, _______, _______, _______, _______, _______,
+    RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_M_SN,_______,                                      _______, _______, _______, _______, _______, _______,
+    RGB_RMOD,RGB_HUD, RGB_SAD, RGB_VAD, RGB_M_K, _______,                                      _______, _______, _______, _______, _______, _______,
+             _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______
+  ),
+
+  // =========================================================
+  // Layer 4: ファンクションキー (F1〜F10) & 数字
+  // =========================================================
+  [4] = LAYOUT_universal(
+    _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                      KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______,
+    _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                                      KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , _______,
+    _______, _______, _______, _______, _______, _______,                                      _______, _______, _______, _______, _______, _______,
+             _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______
+  ),
+
+  // =========================================================
+  // Layer 5: テンキー (Numpad)
+  // =========================================================
+  [5] = LAYOUT_universal(
+    _______, _______, _______, _______, _______, _______,                                      KC_PEQL, KC_P1  , KC_P2  , KC_P3  , _______, _______,
+    _______, _______, _______, _______, _______, _______,                                      KC_PMNS, KC_P4  , KC_P5  , KC_P6  , KC_PPLS, _______,
+    _______, _______, _______, _______, _______, _______,                                      _______, KC_P7  , KC_P8  , KC_P9  , KC_PSLS, _______,
+             _______, _______, _______, _______, _______,                   _______, _______, KC_P0  , KC_PDOT, _______
+  )
+};
 
 void keyboard_post_init_user(void) {
 #ifdef RGBLIGHT_ENABLE
